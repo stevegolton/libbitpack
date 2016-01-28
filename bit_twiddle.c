4d8b4db2c3e027ea208832aac09e182fd1c7edbb
Stephen Golton <stephen.golton@mclaren.com>
2016-01-28 11:48:46 +0000

    Comments, formatting.

--- bit_twiddle.c
+++ bit_twiddle.c
@@ -5,86 +5,76 @@
 
 #include "bit_twiddle.h"
 
-// TODO pack...?
-union test
-{
-	uint32_t val;
-	uint8_t bytes[4];
-};
-
-#define mMASK(x)					( ( x < 64 )?( ( 0x1ULL << x ) - 1 ):0xFFFFFFFFFFFFFFFFULL )
-
-static inline eByteOrder_t GetSystemByteOrder( void );
-
 /**
- * Pulls out a 64-bit unsigned integer from an array of bytes with a given byte
+ * Pulls a 64-bit unsigned integer out of an array of bytes with a given byte
  * order. The result is copied into the passed 64 bit pointer.
  *
- * @param[in]	Byte array to use.
+ * @param[in]	buf				Byte array to use.
  * @param[in]	offset_bits		Offset from the START of the array i.e. the
  * 								least significant bit of element [0] of the
  * 								byte array.
  * @param[in]	width_bits		Width if the parameter in bits.
  * @param[in]	byte_order		The byte order LSB or MSB first.
- * @param[out]	qwDest			Destination int.
+ * @param[out]	dst				Destination int.
  *
  * @return		Error code.
  */
-int UnpackUint64( const uint8_t * const pabyData,
+int UnpackUint64( const uint8_t * const buf,
 				  const size_t offset_bits,
 				  const size_t width_bits,
-				  const eByteOrder_t eByteOrder,
-				  uint64_t * qwDest )
+				  const byte_order_t byte_order,
+				  uint64_t * dest )
 {
-	uint64_t qwTemp = 0;
-	int iIdx;
-	int offset = 0;
+	uint64_t temp = 0;
+	int bit_index;
 	int bitoffset = 0;
 	int byteoffset = 0;
-	int widthinbytes = ( ( width_bits - 1 ) / 8 ) + 1;
+	int width_bytes = ( ( width_bits - 1 ) / 8 ) + 1;	/* Width in bytes rounded up */
+	int targetByte = bit_index / 8;
+	int targetBit = bit_index % 8;
+	uint8_t byte = 0;
 
-	/* Cant do anything more than a uint64 */
+	/* Can't return anything wider than a 64 bit int */
 	if ( width_bits > ( 8 * sizeof( uint64_t ) ) )
+	{
 		return -1;
+	}
 
-	if ( eByteOrder == eByteOrder_LSBFirst )
+	if ( byte_order == eByteOrder_LSBFirst )
 	{
 		/* Little endian */
-		for ( iIdx = offset_bits; iIdx < ( offset_bits + width_bits ); iIdx++ )
+		for ( bit_index = offset_bits; bit_index < ( offset_bits + width_bits ); bit_index++ )
 		{
-			int targetByte = iIdx / 8;
-			int targetBit = iIdx % 8;
+			targetByte = bit_index / 8;
+			targetBit = bit_index % 8;
 
 			/* Work out which byte we want */
-			qwTemp |= ( (uint64_t)( pabyData[targetByte] >> targetBit ) & 0x1 ) << offset++;
+			temp |= ( (uint64_t)( buf[targetByte] >> targetBit ) & 0x1 ) << bitoffset++;
 		}
-
-		*qwDest = qwTemp;
 	}
 	else
 	{
 		/* Big endian */
-		for ( iIdx = offset_bits; iIdx < ( offset_bits + width_bits ); iIdx++ )
+		for ( bit_index = offset_bits; bit_index < ( offset_bits + width_bits ); bit_index++ )
 		{
-			int targetByte = iIdx / 8;
-			int targetBit = iIdx % 8;
-			uint8_t byte;
+			targetByte = bit_index / 8;
+			targetBit = bit_index % 8;
 
 			/* Work out which byte we want */
-			byte |= ( ( pabyData[targetByte] >> targetBit ) & 0x1 ) << bitoffset++;
+			byte |= ( ( buf[targetByte] >> targetBit ) & 0x1 ) << bitoffset++;
 
 			if ( 8 == bitoffset )
 			{
 				/* We have completed a byte to move it up into place */
-				qwTemp |= ((uint64_t)byte) << (8*(widthinbytes - (++byteoffset)));
+				temp |= ((uint64_t)byte) << (8*(width_bytes - (++byteoffset)));
 
 				bitoffset = 0;
 				byte = 0;
 			}
 		}
-
-		*qwDest = qwTemp;
 	}
+
+	*dest = temp;
 
 	return 0;
 }
@@ -93,34 +83,12 @@ int UnpackUint64( const uint8_t * const pabyData,
  * Pulls out a 64-bit unsigned integer from an array of bytes with a given byte
  * order. The result is copied into the passed 64 bit pointer.
  */
-int PackUint64( const uint8_t * const pabyData,
-				const size_t iOffsetBits,
-				const size_t iWidthBits,
-				const eByteOrder_t eByteOrder,
-				const uint64_t qwSrc )
+int PackUint64( const uint8_t * const buf,
+				const size_t offset_bits,
+				const size_t width_bits,
+				const byte_order_t byte_order,
+				const uint64_t src )
 {
-	uint64_t qwTemp = qwSrc;
-
-	/* TODO cant do anything more than 64 bits yet as we cast to a uint64 */
-	if ( ( iOffsetBits + iWidthBits ) > 64 )
-		return -1;
-
+	/* Not implemented yet */
 	return -1;
-}
-
-/* ************************************************************************** */
-static inline eByteOrder_t GetSystemByteOrder( void )
-{
-	union test testval;
-
-	testval.val = 0x1;
-
-	if ( testval.bytes[0] == 0x1 )
-	{
-		return eByteOrder_LSBFirst;
-	}
-	else
-	{
-		return eByteOrder_MSBFirst;
-	}
 }
--- bit_twiddle.h
+++ bit_twiddle.h
@@ -6,19 +6,19 @@ typedef enum
 	eByteOrder_MSBFirst,	/* Motorola */
 	eByteOrder_LSBFirst		/* Intel */
 
-} eByteOrder_t;
+} byte_order_t;
 
-int UnpackUint64( const uint8_t * const pabyData,
-					   const size_t iOffsetBits,
-					   const size_t iWidthBits,
-					   const eByteOrder_t eByteOrder,
-					   uint64_t * qwDest );
+int UnpackUint64( const uint8_t * const buf,
+					   const size_t offset_bits,
+					   const size_t width_bits,
+					   const byte_order_t byte_order,
+					   uint64_t * dest );
 
-int PackUint64( const uint8_t * const pabyData,
-				const size_t iOffsetBits,
-				const size_t iWidthBits,
-				const eByteOrder_t eByteOrder,
-				const uint64_t qwSrc );
+int PackUint64( const uint8_t * const buf,
+				const size_t offset_bits,
+				const size_t width_bits,
+				const byte_order_t byte_order,
+				const uint64_t src );
 
 
 
