cd7e2b208c87592eb0a9847ee241d5cba32aa42d
Steve Golton <stevegolton@gmail.com>
2016-03-16 21:56:55 +0000

    Added packing functions.

--- main.c
+++ main.c
@@ -13,6 +13,8 @@ static void test_unpack_uint64_be_aligned( void **state );
 static void test_unpack_uint64_le_aligned( void **state );
 static void test_unpack_uint64_be_unaligned( void **state );
 static void test_unpack_uint64_le_unaligned( void **state );
+static void test_pack_uint64_le_aligned( void **state );
+static void test_pack_uint64_be_aligned( void **state );
 
 struct test
 {
@@ -32,6 +34,8 @@ int main(void)
 		unit_test( test_unpack_uint64_le_aligned ),
 		unit_test( test_unpack_uint64_be_unaligned ),
 		unit_test( test_unpack_uint64_le_unaligned ),
+		unit_test( test_pack_uint64_le_aligned ),
+		unit_test( test_pack_uint64_be_aligned ),
 	};
 	return run_tests(tests);
 }
@@ -132,11 +136,116 @@ static void test_unpack_uint64_le_unaligned( void **state )
 
 	print_array_raw( input, 8 );
 
-	/* Unpack that big word we just wrote in */
+	/* Unpack that bitest_pack_uint64_le_alignedg word we just wrote in */
 	int err = unpack_uint64_le( input, sizeof( input ), 3, 32, &result );
 
 	assert_int_equal( 0, err );
 	assert_int_equal( expected, result );
+}
+
+static void test_pack_uint64_be_aligned( void **state )
+{
+	const uint8_t expected[8] = { 0x00,
+							   0xBE,
+							   0xEF,
+							   0xFA,
+							   0xCE,
+							   0x00,
+							   0x00,
+							   0x00 };
+	uint8_t result[8] = { 0, };
+	const uint64_t input = 0xBEEFFACE;
+
+	(void) state; /* unused */
+
+	/* Unpack that big word we just wrote in */
+	int err = pack_uint64_be( result, sizeof( result ), 24, 32, input );
+
+	print_array_raw( result, sizeof( result ) );
+
+	assert_int_equal( 0, err );
+	assert_memory_equal( expected, result, sizeof( expected ) );
+}
+
+static void test_pack_uint64_le_aligned( void **state )
+{
+	const uint8_t expected[8] = { 0x00,
+								  0xCE,
+								  0xFA,
+								  0xEF,
+								  0xBE,
+								  0x00,
+								  0x00,
+								  0x00 };
+	uint8_t result[8] = { 0, };
+	const uint64_t input = 0xBEEFFACE;
+
+	(void) state; /* unused */
+
+	/* Unpack that big word we just wrote in */
+	int err = pack_uint64_le( result, sizeof( result ), 8, 32, input );
+
+	print_array_raw( result, sizeof( result ) );
+
+	assert_int_equal( 0, err );
+	assert_memory_equal( expected, result, sizeof( expected ) );
+}
+
+static void test_pack_uint64_be_unaligned( void **state )
+{
+	const uint8_t expected[8] = { 0x00,
+			   0x0B,
+			   0xEE,
+			   0xFF,
+			   0xAC,
+			   0xE0,
+			   0x00,
+			   0x00 };
+	uint8_t result[8] = { 0, };
+	const uint64_t input = 0xBEEFFACE;
+	int err;
+
+	(void) state; /* unused */
+
+	/* Unpack that big word we just wrote in */
+	err = pack_uint64_be( result, sizeof( result ), 20, 32, input );
+
+	print_array_raw( result, sizeof( result ) );
+
+	assert_int_equal( 0, err );
+	assert_memory_equal( expected, result, sizeof( expected ) );
+
+	/* Unpack that big word we just wrote in */
+	err = pack_uint64_be_rev( result, sizeof( result ), 12, 32, input );
+
+	print_array_raw( result, sizeof( result ) );
+
+	assert_int_equal( 0, err );
+	assert_memory_equal( expected, result, sizeof( expected ) );
+}
+
+static void test_pack_uint64_le_unaligned( void **state )
+{
+	const uint8_t expected[8] = { 0x70,
+			   0xd6,
+			   0x7f,
+			   0xf7,
+			   0xfd,
+			   0x7f,
+			   0x00,
+			   0x00 };
+	uint8_t result[8] = { 0, };
+	const uint64_t input = 0xBEEFFACE;
+
+	(void) state; /* unused */
+
+	/* Unpack that big word we just wrote in */
+	int err = pack_uint64_le( result, sizeof( result ), 3, 32, input );
+
+	print_array_raw( result, sizeof( result ) );
+
+	assert_int_equal( 0, err );
+	assert_memory_equal( expected, result, sizeof( expected ) );
 }
 
 void print_array_raw( const uint8_t *bytes, const int len )
